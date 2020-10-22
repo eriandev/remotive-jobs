1ef8548bfc8fffc97190322387a7d3a006f4bf28
erianvc <erianvc.dev@gmail.com>
2020-10-22 16:37:51 -0500

    feat: searchbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule
