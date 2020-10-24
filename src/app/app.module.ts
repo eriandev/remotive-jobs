c6e0b289d007f43dcfe0f24e5f0c326b751b8a43
erianvc <erianvc.dev@gmail.com>
2020-10-24 00:16:49 -0500

    feat: location searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchzoneComponent } from './components/searchzone/searchzone.component';
+import { FilterzoneComponent } from './components/filterzone/filterzone.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchzoneComponent
+    SearchzoneComponent,
+    FilterzoneComponent
   ],
   imports: [
     BrowserModule
