f30066eb17616cabd2114e24135583f5ced7fc81
erianvc <erianvc.dev@gmail.com>
2020-10-25 00:27:15 -0500

    feat: card component complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SearchzoneComponent } from './components/searchzone/searchzone.component';
 import { FilterzoneComponent } from './components/filterzone/filterzone.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchzoneComponent,
-    FilterzoneComponent
+    FilterzoneComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
