15299a96908ee25d32c408e2cc5170bffa069477
erianvc <erianvc.dev@gmail.com>
2022-02-20 12:37:27 -0500

    feat: adds title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'remotive-jobs'`, () => {
+  it(`should have as title 'Remotive'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('remotive-jobs');
+    expect(app.title).toEqual('Remotive');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('remotive-jobs app is running!');
+    expect(compiled.querySelector('h1 strong')?.textContent).toContain('Remotive');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public readonly title = 'remotive-jobs';
+  public readonly title = 'Remotive';
 }
