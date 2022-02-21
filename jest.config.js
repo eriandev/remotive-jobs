62458513a416b7e96d3cc6a288305e84806ea951
erianvc <erianvc.dev@gmail.com>
2022-02-20 19:33:06 -0500

    refactor: adds path alias

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
+  verbose: true,
   moduleNameMapper: {
-    '@app/(.*)': '<rootDir>/src/app/$1'
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@components/(.*)': '<rootDir>/src/app/components/$1',
+    '@environments/(.*)': '<rootDir>/src/environments/$1',
   },
   preset: 'jest-preset-angular',
-  coverageReporters: [
-    'text',
-    'html'
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/setup-jest.ts'
-  ]
+  coverageReporters: ['text', 'html'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
 }
