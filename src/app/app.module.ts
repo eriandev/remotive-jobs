8be602c5fbca721d06f825b34a03bd5b643fbf12
erianvc <erianvc.dev@gmail.com>
2022-03-03 23:24:12 -0500

    feat: adds search zone desgin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SearchZoneComponent } from './components/search-zone/search-zone.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  declarations: [AppComponent, SearchZoneComponent],
+  imports: [BrowserModule, FormsModule, CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/search-zone/search-zone.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchZoneComponent } from './search-zone.component';
+
+describe('SearchZoneComponent', () => {
+  let component: SearchZoneComponent;
+  let fixture: ComponentFixture<SearchZoneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SearchZoneComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchZoneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
