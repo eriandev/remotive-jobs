d8ed267438b557fac4c9d57f04569e48e44feba8
erianvc <erianvc.dev@gmail.com>
2022-03-15 00:04:33 -0500

    feat: adds job card design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { JobCardComponent } from './components/job-card/job-card.component';
 import { SearchZoneComponent } from './components/search-zone/search-zone.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchZoneComponent],
-  imports: [BrowserModule, FormsModule, CoreModule],
+  declarations: [AppComponent, SearchZoneComponent, JobCardComponent],
+  imports: [BrowserModule, FormsModule, RouterModule, CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
