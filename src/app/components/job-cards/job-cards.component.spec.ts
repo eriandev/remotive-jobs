0008f43987c05341e2d4ab2e46f1b04812475427
erianvc <erianvc.dev@gmail.com>
2022-03-31 23:04:43 -0500

    feat: load more jobs when scrolling

--- src/app/components/job-cards/job-cards.component.spec.ts
+++ src/app/components/job-cards/job-cards.component.spec.ts
@@ -1,18 +1,8 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
 
 import { JobCardsComponent } from './job-cards.component';
-import { JobCardComponent } from '@components/job-card/job-card.component';
-import { SearchZoneComponent } from '@components/search-zone/search-zone.component';
-
-@NgModule({
-  declarations: [JobCardComponent, SearchZoneComponent],
-  schemas: [NO_ERRORS_SCHEMA],
-})
-class MyModule {}
-
 describe('JobCardsComponent', () => {
   let component: JobCardsComponent;
   let fixture: ComponentFixture<JobCardsComponent>;
@@ -20,7 +10,7 @@ describe('JobCardsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [JobCardsComponent],
-      imports: [HttpClientTestingModule, FormsModule, MyModule],
+      imports: [HttpClientTestingModule, FormsModule],
     }).compileComponents();
   });
 
@@ -29,7 +19,6 @@ describe('JobCardsComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/components/job-cards/job-cards.component.ts
+++ src/app/components/job-cards/job-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { JobService } from '@core/services/job.service';
 import type { Job } from '@core/interfaces';
@@ -9,12 +9,33 @@ import type { Job } from '@core/interfaces';
   styleUrls: ['./job-cards.component.css'],
 })
 export class JobCardsComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const body = document.body;
+    const scrolled = window.scrollY + window.innerHeight;
+    const limit = Math.max(body.scrollHeight, body.clientHeight);
+    if (limit === scrolled && !this.loading) this.addMoreJobs(this.currentPage);
+  }
+
+  public currentPage = 1;
+  public loading = false;
+  public moreJobs = true;
   public jobs: Job[] = [];
 
   constructor(private readonly jobService: JobService) {}
 
-  async ngOnInit(): Promise<void> {
-    const response = await this.jobService.getJobsByPage();
-    this.jobs = response.results;
+  ngOnInit(): void {
+    this.addMoreJobs();
+  }
+
+  public async addMoreJobs(page?: number): Promise<void> {
+    if (this.moreJobs) {
+      this.loading = true;
+      const response = await this.jobService.getJobsByPage(page);
+      this.jobs = [...this.jobs, ...response.results];
+      this.moreJobs = Boolean(response.next);
+      this.loading = false;
+      this.currentPage++;
+    }
   }
 }
