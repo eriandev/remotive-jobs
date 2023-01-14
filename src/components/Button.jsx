2e77543f35e8ac76cac35e8fccf3d376db989a74
Erick Vargas <erianvc.dev@gmail.com>
2023-01-13 20:43:52 -0500

    refactor: adds state in Input and Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button ({ children }) {
+export default function Button ({ children, onClick }) {
   return (
-    <button className='w-max select-none rounded border-none bg-primary py-3 px-6 font-medium text-white transition-[color,background-color] duration-[250ms] ease-in-out hover:bg-chocolate hover:text-primary md:w-32'>
+    <button
+      className='w-max select-none rounded border-none bg-primary py-3 px-6 font-medium text-white transition-[color,background-color] duration-[250ms] ease-in-out hover:bg-chocolate hover:text-primary md:w-32'
+      onClick={onClick}
+    >
       {children}
     </button>
   )
--- src/components/SearchZone.jsx
+++ src/components/SearchZone.jsx
@@ -1,17 +1,16 @@
-import { ReactComponent as IconBriefcase } from '../assets/briefcase.svg'
+import { useState } from 'react'
+import Input from './Input'
 import Button from './Button'
+import { ReactComponent as IconBriefcase } from '../assets/briefcase.svg'
 
 export default function Search () {
+  const [value, setValue] = useState('')
+
   return (
     <section className='flex h-[8.75rem] w-full flex-wrap items-center justify-center rounded-lg bg-[url(./bg-search-zone.png)] bg-center bg-no-repeat px-4'>
       <article className='grid w-full max-w-[720px] grid-cols-[auto_minmax(80px,100%)_auto] items-center gap-[6px] rounded bg-white p-1 pl-4'>
         <IconBriefcase width='20' height='20' />
-        <input
-          type='text'
-          aria-label='Title, companies, expertise or benefits'
-          placeholder='Title, companies, expertise or benefits'
-          className='placeholder:text-gray block h-6 w-full overflow-hidden text-ellipsis whitespace-nowrap border-none p-1 text-sm text-[#282538]'
-        />
+        <Input.Search placeholder='Title, companies, expertise or benefits' onInput={setValue} />
         <Button>
           Search
         </Button>
