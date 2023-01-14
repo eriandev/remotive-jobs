5e4debea742b35f7408116d8947faf4f4bbaf7b6
Erick Vargas <erianvc.dev@gmail.com>
2023-01-13 22:06:07 -0500

    fix: corrects `FilterZone` design

--- src/components/FilterZone.jsx
+++ src/components/FilterZone.jsx
@@ -5,9 +5,9 @@ export default function FilterZone () {
   const [selected, setSelected] = useState(null)
 
   return (
-    <section className='sticky top-28 rounded-2xl py-4 px-3 border border-[rgb(237,242,247)]'>
+    <section className='sticky top-28 h-max rounded-2xl border border-[rgb(237,242,247)] py-6 px-4'>
       <span className='block font-bold mb-3 leading-5'>Remote Location</span>
-      <article className='grid gap-2 mb-4 grid-rows-3'>
+      <article className='grid grid-rows-3 gap-2'>
         <Input.Radio imgSrc='worldwide-symbol.png' label='Worldwide' name='location' value='Worldwide' onChange={setSelected} />
         <Input.Radio text='🇺🇸' label='USA Only' name='location' value='USA Only' onChange={setSelected} />
         <Input.Radio imgSrc='remote-symbol.png' label='Other Locations' name='location' value='Other Locations' onChange={setSelected} />
