5d3ad7091a59af590f2979899c53c52b21a11436
Erick Vargas <erianvc.dev@gmail.com>
2023-01-15 16:58:53 -0500

    feat: adds button to loading more jobs

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
-export default function Button ({ children, onClick }) {
+export default function Button ({ children, onClick, outline = false }) {
+  const baseStyles = 'w-max select-none rounded py-3 px-6 font-medium transition-[color,background-color] duration-[250ms] ease-in-out md:w-32'
+  const fillStyles = 'border-none bg-primary text-white hover:bg-chocolate hover:text-primary'
+  const outlineStyles = 'border border-primary bg-transparent text-primary'
+
   return (
     <button
-      className='w-max select-none rounded border-none bg-primary py-3 px-6 font-medium text-white transition-[color,background-color] duration-[250ms] ease-in-out hover:bg-chocolate hover:text-primary md:w-32'
+      className={`${baseStyles} ${outline ? outlineStyles : fillStyles}`}
       onClick={onClick}
     >
       {children}
