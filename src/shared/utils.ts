88bb3f13e99977ac88dd0f3388c685134244e412
Erick Vargas <mail@erian.dev>
2026-01-03 19:58:07 -0500

    refactor: move usePipes to utils

---
+++ src/shared/utils.ts
@@ -0,0 +1,34 @@
+const DATE_UNITS = {
+  day: 86400,
+  hour: 3600,
+  minute: 60,
+  second: 1,
+} as const
+
+export function getLocationIcon(requiredLocation: string) {
+  return requiredLocation === 'Worldwide' || requiredLocation === 'Anywhere'
+    ? 'worldwide-symbol.png'
+    : 'remote-symbol.png'
+}
+
+export function getRelativeDate(stringDate: string, locales: string | string[] = 'en') {
+  const date = new Date(stringDate)
+  const rtf = new Intl.RelativeTimeFormat(locales)
+  const secondsElapsed = getSecondsDiff(date.getTime())
+  const { value, unit } = getUnitAndValueDate(secondsElapsed)
+
+  return rtf.format(value, unit as Intl.RelativeTimeFormatUnit)
+}
+
+const getSecondsDiff = (timestamp: number) => (Date.now() - timestamp) / 1000
+
+const getUnitAndValueDate = (secondsElapsed: number) => {
+  for (const [unit, secondsInUnit] of Object.entries(DATE_UNITS)) {
+    if (secondsElapsed >= secondsInUnit || unit === 'second') {
+      const value = Math.floor(secondsElapsed / secondsInUnit) * -1
+      return { value, unit }
+    }
+  }
+
+  return { value: 0, unit: 'second' }
+}
